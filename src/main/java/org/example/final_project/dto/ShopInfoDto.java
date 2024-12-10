c186121301faa9a3406997ef36c93b3f2cda4c63
itsHuaan <doquanghuan1909@gmail.com>
2024-12-10 10:17:47 +0700

    abc

---
+++ src/main/java/org/example/final_project/dto/ShopInfoDto.java
@@ -0,0 +1,10 @@
+package org.example.final_project.dto;
+
+public class ShopInfoDto {
+    private String shopName;
+    private String shopAddress;
+    private String shopAddressDetail;
+    private Long productCount;
+    private String joined;
+    private Long feedbackCount;
+}
