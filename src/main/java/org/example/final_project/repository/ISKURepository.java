451b0bdeeb7a648d724884eb9ed252034238d99e
unknown <duong26122003dddppp@gmail.com>
2024-12-13 14:49:29 +0700

    fix add and delete variants

--- src/main/java/org/example/final_project/repository/ISKURepository.java
+++ src/main/java/org/example/final_project/repository/ISKURepository.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 @Repository
 public interface ISKURepository extends JpaRepository<SKUEntity,Long> {
     List<SKUEntity> findAllByProduct_Id(long productId);
+    List<SKUEntity> findAllByOption1_IdOrOption2_Id(long optionId1,long optionId2);
 
     @Query("select s.product.id from SKUEntity s where s.id = :SkuId")
     long findProductIdbySKUId(long SKUId);
