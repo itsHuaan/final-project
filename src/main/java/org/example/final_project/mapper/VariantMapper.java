7f2048cd87da815b31286540e676f5a45f5d1061
itsHuaan <doquanghuan1909@gmail.com>
2024-12-13 10:38:38 +0700

    cart fix

--- src/main/java/org/example/final_project/mapper/VariantMapper.java
+++ src/main/java/org/example/final_project/mapper/VariantMapper.java
@@ -14,11 +14,16 @@ import org.springframework.stereotype.Component;
 public class VariantMapper {
     IProductOptionValueService valueService;
     ProductMapper productMapper;
-    public CartSkuDto toDto(SKUEntity entity){
+
+    public CartSkuDto toDto(SKUEntity entity) {
         return CartSkuDto.builder()
                 .itemId(entity.getId())
-                .value1(valueService.getById(entity.getValue1().getId()))
-                .value2(valueService.getById(entity.getValue2().getId()))
+                .value1(entity.getOption1() != null
+                        ? valueService.getById(entity.getValue1().getId())
+                        : null)
+                .value2(entity.getOption2() != null
+                        ? valueService.getById(entity.getValue2().getId())
+                        : null)
                 .productFamily(productMapper.toProductFamilyDto(entity.getProduct()))
                 .price(entity.getPrice())
                 .quantity(entity.getQuantity())
