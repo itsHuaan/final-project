7e63909acb33affac667822470314e83882f36e3
itsHuaan <doquanghuan1909@gmail.com>
2024-12-18 23:14:21 +0700

    statistic

--- src/main/java/org/example/final_project/service/impl/StatisticService.java
+++ src/main/java/org/example/final_project/service/impl/StatisticService.java
@@ -4,13 +4,16 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.example.final_project.dto.ShopStatisticDto;
+import org.example.final_project.entity.FeedbackEntity;
+import org.example.final_project.entity.ProductEntity;
 import org.example.final_project.repository.IProductRepository;
 import org.example.final_project.service.IStatisticService;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import static org.example.final_project.specification.ProductSpecification.isNotDeleted;
-import static org.example.final_project.specification.ProductSpecification.ofShop;
+import java.util.List;
+
+import static org.example.final_project.specification.ProductSpecification.*;
 
 @Service
 @RequiredArgsConstructor
@@ -21,8 +24,28 @@ public class StatisticService implements IStatisticService {
     @Override
     public ShopStatisticDto getStatistic(long shopId) {
         return ShopStatisticDto.builder()
+                .averageRating(getAverageOfRating(shopId))
+                .totalOfFeedbacks(getTotalOfFeedbacks(shopId))
                 .totalOfProducts(getTotalProducts(shopId))
                 .build();
+    }
+
+    private double getAverageOfRating(long shopId) {
+        List<ProductEntity> products = productRepository.findAll(Specification.where(ofShop(shopId)).and(isValid())).stream().toList();
+        return products.stream()
+                .mapToDouble(product -> product.getFeedbacks().stream()
+                        .mapToDouble(FeedbackEntity::getRate)
+                        .average()
+                        .orElse(0.0))
+                .average()
+                .orElse(0.0);
+    }
+
+    private int getTotalOfFeedbacks(long shopId) {
+        List<ProductEntity> products = productRepository.findAll(Specification.where(ofShop(shopId)).and(isValid())).stream().toList();
+        return products.stream()
+                .mapToInt(product -> product.getFeedbacks().stream().toList().size())
+                .sum();
     }
 
     private int getTotalProducts(long shopId) {
