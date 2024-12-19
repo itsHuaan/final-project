3e0a35c7d53a8baf8e08021cef3c70e83753df41
itsHuaan <doquanghuan1909@gmail.com>
2024-12-19 15:33:12 +0700

    spec fix

--- src/main/java/org/example/final_project/specification/OrderDetailSpecification.java
+++ src/main/java/org/example/final_project/specification/OrderDetailSpecification.java
@@ -1,10 +1,13 @@
 package org.example.final_project.specification;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import org.example.final_project.entity.OrderDetailEntity;
 import org.springframework.data.jpa.domain.Specification;
 
 public class OrderDetailSpecification {
     public static Specification<OrderDetailEntity> hasShop(long shopId) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("shopId"), shopId);
+        return (Root<OrderDetailEntity> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) -> criteriaBuilder.equal(root.get("shopId"), shopId);
     }
 }
