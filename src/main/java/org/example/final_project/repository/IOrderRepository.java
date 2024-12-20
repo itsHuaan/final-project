f2da657a699c11f865a36997e717f08e57929042
itsHuaan <doquanghuan1909@gmail.com>
2024-12-20 23:15:29 +0700

    statistic

--- src/main/java/org/example/final_project/repository/IOrderRepository.java
+++ src/main/java/org/example/final_project/repository/IOrderRepository.java
@@ -1,6 +1,7 @@
 package org.example.final_project.repository;
 
 import org.example.final_project.entity.OrderEntity;
+import org.example.final_project.entity.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,4 +37,5 @@ public interface IOrderRepository extends JpaRepository<OrderEntity, Long> {
 
     @Query("select o.id from OrderEntity o where o.user.userId = :userId and o.orderCode = :orderCode")
     long findOrderIdByUserIdAndOrderCode(long userId, String orderCode);
+
 }
