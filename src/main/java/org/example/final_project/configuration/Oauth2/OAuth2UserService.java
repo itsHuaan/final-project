ab11d1213b11fc06ede1df1d7c69e721c2fe2bf5
itsHuaan <doquanghuan1909@gmail.com>
2024-12-23 08:10:13 +0700

    fix login

--- src/main/java/org/example/final_project/configuration/Oauth2/OAuth2UserService.java
+++ src/main/java/org/example/final_project/configuration/Oauth2/OAuth2UserService.java
@@ -2,16 +2,12 @@ package org.example.final_project.configuration.Oauth2;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.example.final_project.configuration.jwt.JwtProvider;
-import org.example.final_project.dto.SignInResponse;
 import org.example.final_project.entity.RoleEntity;
 import org.example.final_project.entity.UserEntity;
-import org.example.final_project.mapper.UserMapper;
 import org.example.final_project.repository.IUserRepository;
 import org.example.final_project.util.Const;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
@@ -34,19 +30,20 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 public class OAuth2UserService extends DefaultOAuth2UserService {
-    private final  IUserRepository userRepository;
+    private final IUserRepository userRepository;
     private final JwtProvider jwtProvider;
+
     @Override
     //chạy hàm này đầu tiên
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         //load thông tin từ phương thức đăng nhập
         OAuth2User oAuth2User = super.loadUser(userRequest);
-        String provideName = null ;
-        OAuth2UserCustom userCustom =OAuth2UserCustom.builder().build();
-        if(userRequest.getClientRegistration().getClientName().equals("google")) {
+        String provideName = null;
+        OAuth2UserCustom userCustom = OAuth2UserCustom.builder().build();
+        if (userRequest.getClientRegistration().getClientName().equals("google")) {
             provideName = Const.GOOGLE;
             userCustom = getUserPropertiesForGoogle(oAuth2User);
-        }else if(userRequest.getClientRegistration().getClientName().equals("facebook")) {
+        } else if (userRequest.getClientRegistration().getClientName().equals("facebook")) {
             provideName = Const.FACEBOOK;
             userCustom = getUserPropertiesForFaceBook(oAuth2User);
         }
@@ -54,11 +51,11 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
         userCustom.setAuthorities(AuthorityUtils.createAuthorityList("ROLE_BUYER"));
         userCustom.setAccessToken(userRequest.getAccessToken().getTokenValue());
         userCustom.setExpiresAt(userRequest.getAccessToken().getExpiresAt());
-        UserEntity user =userRepository.findByEmail(userCustom.getEmail()).orElse(null);
-        if(user == null) {
+        UserEntity user = userRepository.findByEmail(userCustom.getEmail()).orElse(null);
+        if (user == null) {
             user = UserEntity.builder()
                     .email(userCustom.getEmail())
-                    .role(new RoleEntity(2L,userCustom.getAuthorities().toString()))
+                    .role(new RoleEntity(2L, userCustom.getAuthorities().toString()))
                     .shop_status(0)
                     .provider(provideName)
                     .name(userCustom.getName())
@@ -66,35 +63,39 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
                     .username(userCustom.getName())
                     .isActive(1)
                     .build();
+        } else {
+            user.setDeletedAt(null);
+            user.setIsActive(1);
         }
         userRepository.save(user);
         return oAuth2User;
     }
 
-    public AuthenticationFailureHandler onFailureHandler(){
+    public AuthenticationFailureHandler onFailureHandler() {
         return ((request, response, authentication) -> {
             response.setContentType(MediaType.APPLICATION_JSON_VALUE);
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             final Map<String, Object> body = new HashMap<>();
-            body.put("timestamp",new Date());
+            body.put("timestamp", new Date());
             body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
-            body.put("error","Bad credentials Oauth2");
-            body.put("message","Invalid username or password");
+            body.put("error", "Bad credentials Oauth2");
+            body.put("message", "Invalid username or password");
             body.put("path", request.getRequestURI());
             final ObjectMapper mapper = new ObjectMapper();
             mapper.writeValue(response.getOutputStream(), body);
         });
     }
-    public AuthenticationSuccessHandler onSuccessHandler(){
-        return((request, response, authentication) -> {
+
+    public AuthenticationSuccessHandler onSuccessHandler() {
+        return ((request, response, authentication) -> {
             OAuth2AuthenticationToken oauth2Authentication = (OAuth2AuthenticationToken) authentication;
             String providerName = oauth2Authentication.getAuthorizedClientRegistrationId();
             DefaultOAuth2User defaultOAuth2User = (DefaultOAuth2User) authentication.getPrincipal();
             String email = null;
-            if (providerName.equals("google")){
+            if (providerName.equals("google")) {
                 email = defaultOAuth2User.getAttributes().get("email").toString();
             }
-            if(providerName.equals("facebook")){
+            if (providerName.equals("facebook")) {
                 email = defaultOAuth2User.getAttributes().get("email").toString();
             }
             UserEntity user = userRepository.findByEmail(email).orElse(null);
@@ -126,6 +127,7 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
 //            response.getWriter().flush();
         });
     }
+
     private OAuth2UserCustom getUserPropertiesForGoogle(OAuth2User oAuth2User) {
         Assert.notNull(oAuth2User.getAttribute("sub"), "google's id");
         Assert.notNull(oAuth2User.getAttribute("name"), "google's name");
@@ -139,6 +141,7 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
                 .thumbnail(oAuth2User.getAttribute("picture"))
                 .build();
     }
+
     private OAuth2UserCustom getUserPropertiesForFaceBook(OAuth2User oAuth2User) {
 
         Assert.notNull(oAuth2User.getAttribute("name"), "facebook's name");
