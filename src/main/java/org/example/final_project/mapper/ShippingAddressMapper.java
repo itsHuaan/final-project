7694a20e8b6737d350f86b3066c213a2d9b5a391
itsHuaan <doquanghuan1909@gmail.com>
2024-12-26 10:57:50 +0700

    address update

--- src/main/java/org/example/final_project/mapper/ShippingAddressMapper.java
+++ src/main/java/org/example/final_project/mapper/ShippingAddressMapper.java
@@ -5,19 +5,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.example.final_project.dto.ShippingAddressDto;
 import org.example.final_project.entity.UserShippingAddressEntity;
-import org.example.final_project.service.IShippingAddressService;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class ShippingAddressMapper {
-    IShippingAddressService shippingAddressService;
+    AddressMapper addressMapper;
 
     public ShippingAddressDto toDto(UserShippingAddressEntity userShippingAddressEntity) {
         return ShippingAddressDto.builder()
-                .id(userShippingAddressEntity.getId())
-                .addressLine1(shippingAddressService.buildAddressLine1(userShippingAddressEntity.getAddress()))
+                .id(userShippingAddressEntity.getAddress().getId())
+                .addressLine1(addressMapper.buildAddressLine(userShippingAddressEntity.getAddress()))
                 .addressLine2(userShippingAddressEntity.getAddressLine2())
                 .build();
     }
--- src/main/java/org/example/final_project/service/IShippingAddressService.java
+++ src/main/java/org/example/final_project/service/IShippingAddressService.java
@@ -1,11 +1,9 @@
 package org.example.final_project.service;
 
 import org.example.final_project.dto.ShippingAddressDto;
-import org.example.final_project.entity.AddressEntity;
 
 import java.util.List;
 
-public interface IShippingAddressService{
+public interface IShippingAddressService {
     List<ShippingAddressDto> getShippingAddresses(Long userId);
-    String buildAddressLine1(AddressEntity address);
 }
